5411efaa3c11cf47806ebf2dbb685d6b9640fccb
Joseph Cohen <josephmco@gmail.com>
2014-04-03 22:26:13 -0700

    Added Index and Styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Futurecorp::Application.routes.draw do
+
+
+  root "welcome#index"
+
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +59,4 @@ Futurecorp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
