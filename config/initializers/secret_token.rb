f852f35c68c311f6381ec58753d7e5b06f57f459
Joseph Cohen <josephmco@gmail.com>
2014-04-03 21:56:27 -0700

    Initial Rails App Created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Futurecorp::Application.config.secret_key_base = 'c810c9e15be9658a36668b39005203286252bee6df38d004ae4b471c8254b4d251b596a7dc7dfeb850a900869961a5a90f10b015bf2089df3d10e7da1714f84b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Futurecorp::Application.config.session_store :cookie_store, key: '_futurecorp_session'
