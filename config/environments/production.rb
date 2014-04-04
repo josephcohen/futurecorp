e1bb607481a86cedb41eb481fd19aaea187e686d
Joseph Cohen <josephmco@gmail.com>
2014-04-04 00:25:34 -0700

    Revert 9a0237a..cf3410e
    This rolls back to commit 9a0237afb97ec518142fc1ba602c0dbdbfe52eaf.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Futurecorp::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -37,7 +37,7 @@ Futurecorp::Application.configure do
 
 
   ##
-  config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
+  config.assets.precompile += ['index.css']
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
